a0732b087407d0ada65ccd3ea18d73b642da0108
pawandarji98 <pawandarji98@gmail.com>
2024-02-27 21:56:33 +0530

    Adding new daily needed date function

--- src/functions.ts
+++ src/functions.ts
@@ -1,35 +1,51 @@
-export function returnCommonRecords(array1:any[], array2:any[], key:string) {
-    const commonRecords:any = [];
-    const keySet = new Set(array2.map((obj:any) => obj[key]));
-    array1.forEach((obj:any) => {
-        if (keySet.has(obj[key])) {
-            commonRecords.push(obj);
-        }
-    });
-    
-    return commonRecords;
-}
+import { dateOptions, timeOptions, dateTimeOptions } from "./types/type-master";
 
-export function removeDuplicates(array:any[], key:string) {
-    const seen = new Set();
-    return array.filter((obj:any) => {
-        const keyValue = obj[key];
-        if (seen.has(keyValue)) {
-            return false;
-        } else {
-            seen.add(keyValue);
-            return true;
-        }
-    });
-}
-
-export function filterArrayByKeyValue(array:any[], key:string, value:any) {
-    const filteredArray = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i][key] === value) {
-            filteredArray.push(array[i]);
-        }
+// Array Functions
+export function returnCommonRecords(array1: any[], array2: any[], key: string) {
+  const commonRecords: any = [];
+  const keySet = new Set(array2.map((obj: any) => obj[key]));
+  array1.forEach((obj: any) => {
+    if (keySet.has(obj[key])) {
+      commonRecords.push(obj);
     }
-    return filteredArray;
+  });
+
+  return commonRecords;
 }
 
+export function removeDuplicates(array: any[], key: string) {
+  const seen = new Set();
+  return array.filter((obj: any) => {
+    const keyValue = obj[key];
+    if (seen.has(keyValue)) {
+      return false;
+    } else {
+      seen.add(keyValue);
+      return true;
+    }
+  });
+}
+
+export function dateManipulate(
+  date: string,
+  dateOnly?: boolean,
+  timeOnly?: boolean,
+  dateTime?: boolean
+) {
+  let currentDate = new Date(date);
+  if (dateOnly) {
+    const formattedDate = currentDate.toLocaleString("en-US", dateOptions);
+    let parts = formattedDate.split(" ");
+    return `${parts[1]} ${parts[0]}, ${parts[2]}`;
+  }
+
+  if (dateTime) {
+    const formattedDate = currentDate.toLocaleString("en-US", dateTimeOptions);
+    return formattedDate;
+  }
+
+  if (timeOnly) {
+    const formattedDate = currentDate.toLocaleString("en-US", timeOptions);
+    return formattedDate;
+  }
+}
---
+++ src/types/type-master.ts
@@ -0,0 +1,24 @@
+export const dateTimeOptions: any = {
+  //Feb 27, 2024, 03:11:48 PM
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: true,
+};
+
+export const dateOptions: any = {
+  // "27 Feb 2024"
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+};
+
+export const timeOptions: any = {
+  // 03:37 PM
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true,
+};
